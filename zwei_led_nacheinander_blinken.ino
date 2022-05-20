57c69221c0351e8b64f93b657d276ad3239c15f1
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-20 14:24:51 +0200

    20052022

--- zwei_led_nacheinander_blinken.ino
+++ zwei_led_nacheinander_blinken.ino
@@ -1,16 +1,16 @@
-int led_red = 8;
+int led_blue = 8;
 int led_green = 13;
 
 void setup() {
-  pinMode(led_red,OUTPUT);
+  pinMode(led_blue,OUTPUT);
   pinMode(led_green,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led_red,HIGH);
+  digitalWrite(led_blue,HIGH);
   digitalWrite(led_green,LOW);
   delay(500);
-  digitalWrite(led_red,LOW);
+  digitalWrite(led_blue,LOW);
   digitalWrite(led_green,HIGH);
   delay(500);
 }
